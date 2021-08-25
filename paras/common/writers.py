c6fe932b7b3b923577e7eb355b6b054843d96056
Terlouw, Barbara <barbara.terlouw@wur.nl>
2021-08-25 14:16:43 +0200

    Backup

--- paras/common/writers.py
+++ paras/common/writers.py
@@ -1,14 +1,12 @@
 def write_tabular(dictionaries, labels, out_file):
     keys = sorted(list(dictionaries[0].keys()))
-    header = '\t'.join(labels) + '\n'
+    header = 'Domain' + '\t'.join(labels) + '\n'
 
     with open(out_file, 'w') as out:
         out.write(header)
         for key in keys:
+            out.write(key)
             for i, dictionary in enumerate(dictionaries):
-                if i == 0:
-                    out.write(dictionary[key])
-                else:
-                    out.write(f'\t{dictionary[key]}')
+                out.write(f'\t{dictionary[key]}')
             out.write('\n')
 
--- paras/domain_extraction/read_positions.py
+++ paras/domain_extraction/read_positions.py
@@ -5,6 +5,8 @@ import paras.data
 
 APOSITION_FILE = os.path.join(os.path.dirname(paras.data.__file__), 'Apositions.txt')
 APOSITION_FILE_34 = os.path.join(os.path.dirname(paras.data.__file__), 'Apositions_34.txt')
+APOSITION_FILE_HMM = os.path.join(os.path.dirname(paras.data.__file__), 'Apositions_hmm.txt')
+APOSITION_FILE_34_HMM = os.path.join(os.path.dirname(paras.data.__file__), 'Apositions_34_hmm.txt')
 START_POSITION = 66
 
 
@@ -50,5 +52,29 @@ def get_reference_positions(positions: List[int], aligned_reference: str) -> Lis
     return pos_list
 
 
+def get_reference_positions_hmm(positions: List[int], aligned_reference: str) -> List[int]:
+    """
+    Adjusts a list of positions to account for gaps in the reference sequence
+
+    Input:
+    positions: list of [int, ->], with integers representing positions of interest in
+        the reference sequence
+    aligned_reference: the (aligned) reference sequence
+
+    Output:
+    pos_list: list of [int, ->], a new list of positions, each >= the original position
+    """
+    pos_list = []
+    position = 0
+    for i, aa in enumerate(aligned_reference):
+        if aa != ".":
+            if position in positions:
+                pos_list.append(i)
+            position += 1
+
+    return pos_list
+
+
 POSITIONS_STACH = read_positions(APOSITION_FILE, START_POSITION)
 POSITIONS_34 = read_positions(APOSITION_FILE_34, START_POSITION)
+
